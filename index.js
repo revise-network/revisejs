74b58e23d3de05732f14e42fd4a189c547b1feda
Anil Dukkipatty <anildukkipatty@gmail.com>
2022-11-16 15:58:06 +0530

    pseudo code added

--- index.js
+++ index.js
@@ -6,14 +6,32 @@ if (args.length <= 2) {
 }
 const command = args[2];
 if (command === 'print-key') {
+  // check if .revisejs folder is created
+  // if not: create it in the users home directory
+  // check if key is created
+  // if not: print "no key found.. please make one"
   console.log(`Your API Key is `);
   return
 }
 if (command === 'generate-key') {
+  // check if .revisejs folder is created
+  // if not: create it in the users home directory
+  // check if key is created
+  // if not: create it
   console.log(`New Key generated. Your API Key is `);
   return
 }
 if (command === 'start') {
+  // check if .revisejs folder is created
+  // if not: create it in the users home directory
+  // check if there is a .env file there
+  // if not: create it
+  // check if the sqlite file is created
+  // if not: create it
+  // check if key is created
+  // if not: create it
+
+  // starting the revisejs server
   require('revisejs');
   return
 }
