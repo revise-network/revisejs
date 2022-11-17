d3aaf1a76baa9fc2453db8a0878478a13312ded6
Anil D <anil@revise.network>
2022-11-17 21:47:27 +0530

    default config added

---
+++ default-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    NODE_ENV: 'development',
+    PORT: "3000",
+    CORS_ORIGIN: "true",
+    CORS_CREDENTIALS: "true",
+    SECRET: "somesecret",
+    JWT_KEY: "somesecret",
+    FRONTEND_URI: "http://localhost:3001",
+    DATABASE_URL: ""
+}
